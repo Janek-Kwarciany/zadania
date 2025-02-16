43ebe6c576b1df4bdabf1bf6edc9790664472fc3
janek <janekkwarciany@gmail.com>
2025-02-16 18:03:54 +0100

    dodanie informacje o wybranej operacji

--- zadanie1.py
+++ zadanie1.py
@@ -3,7 +3,7 @@ while 1:
     polecenie=input()
     if polecenie=="quit":
         print("wyjście z programu")
-        break
+        break    
     if polecenie=="calc":
         print('tryb kalkulatora')
         while 1:
@@ -12,40 +12,45 @@ while 1:
             if polecenie_w_trybie_calc=="q":
                 break
             if polecenie_w_trybie_calc=="+":
+                print('Wbrano operację dodawania')
                 print('podaj liczbę a')
                 a=input()
                 print('podaj liczbę b')
                 b=input()
-                c=int(a)+int(b)
+                c=float(a)+float(b)
                 print('wynik')
                 print(c)
             if polecenie_w_trybie_calc=='-':
+                print('Wbrano operację odejmowanie')
                 print('podaj liczbę a')
                 a=input()
                 print('podaj liczbę b')
                 b=input()
-                c=int(a)-int(b)
+                c=float(a)-float(b)
                 print('wynik')
                 print(c)
             if polecenie_w_trybie_calc=='*':
+                print('Wbrano operację mnożenie')
                 print('podaj liczbę a')
                 a=input()
                 print('podaj liczbę b')
                 b=input()
-                c=int(a)*int(b)
+                c=float(a)*float(b)
                 print('wynik')
                 print(c)
             if polecenie_w_trybie_calc=='/':
+                print('Wbrano operację dzielenie')
                 print('podaj liczbę a')
                 a=input()
                 print('podaj liczbę b')
                 b=input()
-                c=int(a)/int(b)
+                c=float(a)/float(b)
                 print('wynik')
                 print(c)
             if polecenie_w_trybie_calc=='!':
+                print('Wbrano operację silnia') 
                 print('podaj liczbę n')
-                n=int(input())
+                n=float(input())
                 fact=1
                 for i in range(1, n+1):
                     print(fact)
@@ -53,10 +58,13 @@ while 1:
                 print('wynik')
                 print(fact)
             if polecenie_w_trybie_calc=='^':
+                print('Wbrano operację potegowanie') 
                 print('podaj liczbę a')
                 a=input()
                 print('podaj liczbę b')
                 b=input()
-                c=int(a)**int(b)
+                c=float(a)**float(b)
                 print('wynik')
                 print(c)
+    if polecenie=='files':
+        print('Przeglądarka plików')
